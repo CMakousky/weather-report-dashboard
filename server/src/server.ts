b98f7be7099f0a064d15c788cfdb93032c76c7be
Christopher Makousky <christopher.makousky@gmail.com>
2024-11-12 01:19:11 -0600

    htmlRoutes.ts route to serve index.html defined

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { type Request, type Response } from 'express';
 dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
+import { parse } from 'path';
+
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -11,7 +17,13 @@ const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
 
+app.use(express.static('../client/dist'));
+app.get('*', (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
+
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(parse);
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
