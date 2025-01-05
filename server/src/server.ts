5c3f6997b8c4d015410db1a63843fc04384a430f
Christopher Makousky <christopher.makousky@gmail.com>
2025-01-04 21:53:06 -0600

    Fixed server/src/server.ts and changed the 'TODO' tags to 'TODO-COMPLETE'. Added 'exclude': [dist] to the top level tsconfig.json. Filled out some of the TODOs in server/src/service/weatherService.ts. Changed the 'TODO' tag in server/src/routes/htmlRoutes.ts to 'TODO-COMPLETE'.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,22 @@
 import dotenv from 'dotenv';
-import express, { type Request, type Response } from 'express';
+import express from 'express';
 dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
-import { parse } from 'path';
-
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
-
+// TODO-COMPLETE: Serve static files of entire client dist folder
 app.use(express.static('../client/dist'));
-app.get('*', (_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-});
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
-app.use(parse);
+// TODO-COMPLETE: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// TODO: Implement middleware to connect the routes
+// TODO-COMPLETE: Implement middleware to connect the routes
 app.use(routes);
 
 // Start the server on the port
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,50 +1,130 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-// TODO: Define an interface for the Coordinates object
+// TODO-COMPLETE: Define an interface for the Coordinates object
+interface Coordinates {
+  cityName: string;
+  latCoord: number;
+  longCoord: number;
+}
 
-// TODO: Define a class for the Weather object
+// TODO-COMPLETE: Define a class for the Weather object
+class Weather {
+  temp:number;
+  wind:number;
+  humidity:number;
+
+  constructor(temp:number, wind:number, humidity:number) {
+    this.temp = temp;
+    this.wind = wind;
+    this.humidity = humidity;
+  }
+}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
-  // TODO: Define the baseURL, API key, and city name properties
-  const baseURL = process.env.API_BASE_URL;
-  const keyAPI = process.env.API_KEY;
-  let cityName: string= '';
+  // TODO-COMPLETE: Define the baseURL, API key, and city name properties
+  private baseURL?: string; //https://api.openweathermap.org
+  private keyAPI?: string;
+  private cityName: string;
 
-  // TODO: Create fetchLocationData method
-  async function fetchLocationData() {
-    const url = "https://example.org/products.json";
+  constructor(cityName: string) {
+    this.baseURL = process.env.API_BASE_URL || '';
+    this.keyAPI = process.env.API_KEY || '';
+    this.cityName = cityName;
+  }
+
+  // TODO-COMPLETE: Create fetchLocationData method
+  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
     try {
-      const response = await fetch(url);
+      const response = await fetch(`${this.baseURL}/geo/1.0/direct?q=${query},US&limit=1&appid=${this.keyAPI}`);
+
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      console.log(json);
-    } catch (error) {
+      };
+
+      const locationData = await response.json();
+      console.log(locationData);
+
+      return locationData;
+
+    } catch (error:any) {
       console.error(error.message);
     }
   }
 
-  // private async fetchLocationData(query: string) {}
-  // TODO: Create destructureLocationData method
+  // TODO-COMPLETE: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
-  // TODO: Create buildGeocodeQuery method
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    const { cityName, latCoord, longCoord } = locationData;
+    return {cityName, latCoord, longCoord};
+  }
+
+  // TODO-COMPLETE: Create buildGeocodeQuery method
   // private buildGeocodeQuery(): string {}
-  // TODO: Create buildWeatherQuery method
+  private buildGeocodeQuery(locationData: Coordinates): string {
+    const geocodeQuery: string = JSON.stringify(locationData);
+    return geocodeQuery;
+  }
+
+  // TODO-COMPLETE: Create buildWeatherQuery method
   // private buildWeatherQuery(coordinates: Coordinates): string {}
-  // TODO: Create fetchAndDestructureLocationData method
+  private buildWeatherQuery(coordinates: Coordinates): string {
+    const weatherQuery: string = JSON.stringify(coordinates);
+    return weatherQuery;
+  }
+
+  // TODO-COMPLETE: Create fetchAndDestructureLocationData method
   // private async fetchAndDestructureLocationData() {}
-  // TODO: Create fetchWeatherData method
+  private async fetchAndDestructureLocationData() {
+    const locationData: Coordinates = await this.fetchLocationData(this.cityName);
+    const destructuredData: Coordinates = this.destructureLocationData(locationData);
+    return destructuredData;
+  }
+
+  // TODO-COMPLETE: Create fetchWeatherData method
   // private async fetchWeatherData(coordinates: Coordinates) {}
+  private async fetchWeatherData(coordinates: Coordinates) {
+    try {
+      const response = await fetch(`${this.baseURL}/data/2.5/forecast?lat=${coordinates.latCoord}&lon=${coordinates.longCoord}&appid=${this.keyAPI}`);
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      };
+
+      const weatherData = await response.json();
+      console.log(weatherData);
+
+      return weatherData;
+
+    } catch (error:any) {
+      console.error(error.message);
+    }
+  }
+  
+
   // TODO: Build parseCurrentWeather method
   // private parseCurrentWeather(response: any) {}
+  private parseCurrentWeather(response: any) {
+    const parsedResponse = JSON.parse(response);
+    return parsedResponse;
+  }
+
   // TODO: Complete buildForecastArray method
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
+    const weatherArray = [currentWeather, ...weatherData];
+    return weatherArray;
+  }
+  
   // TODO: Complete getWeatherForCity method
   // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) {
+    const locationData = await this.fetchAndDestructureLocationData();
+    const weatherData = await this.fetchWeatherData(locationData);
+
+  }
 }
 
 export default new WeatherService();
